dd6d60186742ce2f984fc9cd3698889be1aa342f
jazcollins <jasmine@mosaicml.com>
2023-09-06 20:14:47 +0000

    pass cross_attention_norm in cross_attention_kwargs

--- diffusion/models/models.py
+++ diffusion/models/models.py
@@ -90,9 +90,6 @@ def stable_diffusion_xl(
         # note: local only
         # config[0]['block_out_channels'] = [32, 32, 1280]  # make smaller and more manageable for local debug
 
-        # add cross attention norm!
-        config[0]['cross_attention_norm'] = 'group_norm'
-
         unet = UNet2DConditionModel(**config[0])
 
         # # smaller SDXL-style unet for debugging
